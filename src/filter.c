6cc3e84ae1c8c8d6a4336aed3b2cef06fc1d44fe
enzzodani <gomes.enzodani@gmail.com>
2024-06-09 15:47:37 -0300

    function definition basic

---
+++ src/filter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Packet Packet;
+struct Packet {
+  int port;
+};
+
+int portFilter (Packet *packet) {
+
+}
+
+void portPrinter(Packet *packet) {
+
+}
